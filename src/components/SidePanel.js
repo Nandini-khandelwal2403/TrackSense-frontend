387769dc5d25e2f5fb2ef29e89c5ee1d1165f027
Nandini-khandelwal2403 <nandinikhandelwal20.set@modyuniversity.ac.in>
2024-03-24 21:40:09 +0530

    add expense button

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -1,23 +1,34 @@
-import React from 'react'
-import { Card, Col } from 'reactstrap'
+import React, { useState } from 'react'
+import { Card, Col , Button } from 'reactstrap'
+import ShowForm from './ShowForm';
 import './SidePanel.css'
 
 function getDate() {
     const today = new Date();
-    // const month = today.getMonth() + 1;
-    // const year = today.getFullYear();
-    // const date = today.getDate();
     const options = { month: 'long', day: 'numeric'};
     const formattedDate = today.toLocaleDateString('en-US', options);
     return formattedDate;
 }
 
 function SidePanel() {
+    const [ showForm, setShowForm] = useState(false);
+
+    
   return (
     <Col id="sidePanel" xs="3">
         <Card className='date'>
             <span>{getDate()}</span>
         </Card>
+        <div className='addExpense'>
+            <Button color="info" className='expenseForm' 
+                onClick={() => {
+                    setShowForm(true)
+                }}
+        >
+                Add Expense
+            </Button>
+            {showForm && <ShowForm closeModal={setShowForm}/>}
+        </div>
     </Col>
   )
 }
